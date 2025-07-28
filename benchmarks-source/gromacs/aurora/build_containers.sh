8e7f11459c2f9f59d2ae01e49fa28004c5c5cc21
Alok Kamatar <alokvk2@aurora-uan-0012.hostmgmt.cm.aurora.alcf.anl.gov>
2025-07-28 21:43:24 +0000

    Fixing and testing aurora scripts

--- benchmarks-source/gromacs/aurora/build_containers.sh
+++ benchmarks-source/gromacs/aurora/build_containers.sh
@@ -4,5 +4,5 @@ ARTIFACT_REPOSITORY=${ARTIFACT_REPOSITORY:-"spcleth/xaas-artifact"}
 
 echo "Building container ${ARTIFACT_REPOSITORY}:gromacs-aurora-specialized"
 
-docker build -t $ARTIFACT_REPOSITORY:gromacs-aurora-specialized -f ./build_scripts/specialized_container/Dockerfile -
+docker build -t $ARTIFACT_REPOSITORY:gromacs-aurora-specialized -f ./build_scripts/container-specialized/Dockerfile -
 docker push $ARTIFACT_REPOSITORY:gromacs-aurora-specialized
