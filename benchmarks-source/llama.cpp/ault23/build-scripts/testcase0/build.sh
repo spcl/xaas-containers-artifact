e026b6088323027b3df7d89b7d1b0311c82cb9a6
Marcin Copik <mcopik@gmail.com>
2025-07-28 05:56:02 +0200

    Update script

--- benchmarks-source/llama.cpp/ault23/build-scripts/testcase0/build.sh
+++ benchmarks-source/llama.cpp/ault23/build-scripts/testcase0/build.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
-#SBATCH --job-name=build_llamma          # Job name
+#SBATCH --job-name=build_llama          # Job name
 #SBATCH --output=build_llama.out
 #SBATCH --error=build_llama.err
 #SBATCH --time=04:00:00                # Time limit
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4              # 4 CPU cores
+#SBATCH --cpus-per-task=64              # 4 CPU cores
 #SBATCH --mem=16G
 #SBATCH --nodelist=ault23              # Specific node
 #SBATCH --gres=gpu:1                   # Request 1 GPU
 
-# Load modules (not used in this test case but fine to pre-load)
+ARTIFACT_LOCATION=${ARTIFACT_LOCATION:-${SCRATCH}/xaas-containers-artifact}
+
 module load cuda/12.1.1 intel-oneapi-mpi/2021.3.0 git/2.10.1 intel-oneapi-mkl/2021.3.0
 
+source ${ARTIFACT_LOCATION}/dependencies/spack/share/spack/setup-env.sh
+spack load gcc@11.5 arch=linux-centos8-zen
+spack load gcc@11.5 
 
-# Set working directory path
-BUILD_DIR="$SCRATCH/llama-builds/testcase0"
+# Navigate to the build directory
+cd ${ARTIFACT_LOCATION}/benchmarks-source/llama.cpp/ault23/build-scripts/testcase0
 
-# Create build directory
-mkdir -p "$BUILD_DIR"
-cd "$BUILD_DIR" || exit 1
-
-# Clone llama.cpp into this directory
-if [ ! -d "$BUILD_DIR/llama.cpp" ]; then
-    git clone https://github.com/ggml-org/llama.cpp
-fi
-
-cd llama.cpp || exit 1
+rm -rf build && mkdir -p build
 
 # Naive build (CPU only, no tuning)
-cmake -B build
-cmake --build build --config Release -j 8
+# Source: https://github.com/ggml-org/llama.cpp/blob/master/docs/build.md
+cd build && cmake ${ARTIFACT_LOCATION}/data/llama.cpp/llama.cpp
+cmake --build . --config Release -j $(nproc)
