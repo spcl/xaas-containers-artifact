352a1720a08b1e587c7fa46aa2e3e5b5fb7a59ff
Marcin Copik <mcopik@gmail.com>
2025-09-14 09:24:49 +0200

    Remove unnecessary push

--- benchmarks-source/llama.cpp/ault23/push_containers.sh
+++ benchmarks-source/llama.cpp/ault23/push_containers.sh
@@ -3,4 +3,3 @@
 DOCKER_REPOSITORY=${DOCKER_REPOSITORY:-"spcleth/xaas-artifact"}
 
 docker push ${DOCKER_REPOSITORY}:llamacpp-specialized-ault23
-docker push ${DOCKER_REPOSITORY}:llamacpp-source-deploy-ault23
